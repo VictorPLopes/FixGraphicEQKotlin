248f63c2c38dc3c2707b8c7a78c82b04dc98bd18
VictorPLopes <victor.probio@gmail.com>
2023-07-27 15:44:03 -0300

    Fixed the source files

--- src/main/kotlin/FrequencyGain.kt
+++ src/main/kotlin/FrequencyGain.kt
@@ -1,2 +1,5 @@
-class FrequencyGain {
-}
+// Class to store frequency and gain values, simplest way I could think of to store an int with a float
+data class FrequencyGain (
+    val frequency: Int,
+    val gain: Float
+)
